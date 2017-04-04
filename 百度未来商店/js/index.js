b504af63571e6273635634b2bbe89c04e292c78d
wangjingyuwangjingyu <1692706952@qq.com>
2017-04-04 12:31:11 +0800

    'dsihgfjhdgsa'

--- 百度未来商店/js/index.js
+++ 百度未来商店/js/index.js
@@ -4,16 +4,18 @@ window.setInterval(function(){
 	$('.boxl').css({'height':b+'px'});
 	$('.bigbox').css({'height':b+'px'});
 	$('.imglunbo').css({'height':b+'px'});
-
 	var a1=Math.floor($('.boxl').width());
-
 	$('.ydbox').css({'width':a1+'px','height':b+'px'});
 	$('.xzbox').css({'width':a1*4+'px','height':b+'px'});
 },200);
-
 // 轮播
-// window.setInterval(function(){
-// 	var a=Math.floor($('.boxl').width());
-// 	$('.xzbox').animate({'left':-33.33333+'%'});
-// },10);
+var i=0;
+window.setInterval(function(){
+	if(i>=4){
+		i=0;
+	}
+	var a=Math.floor($('.boxl').width());
+	$('.xzbox').animate({'left':-100*i+'%'});
+	i++;
+},2000);
 
